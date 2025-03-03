adf5970be35099e5e558dd2553bb4734eb99d59a
Vanessa Tejerina <vanetejerina314@gmail.com>
2025-03-03 15:15:17 -0400

    added alianzas page

--- src/app/pages/alianzas/alianzas.component.ts
+++ src/app/pages/alianzas/alianzas.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ImageBannerComponent } from "../../components/image-banner/image-banner.component";
+import { InfiniteCarouselComponent } from "../../components/infinite-carousel/infinite-carousel.component";
+import { alianzas } from '../data/alianzas.data';
 
 @Component({
   selector: 'app-alianzas',
-  imports: [],
+  imports: [ImageBannerComponent, InfiniteCarouselComponent],
   templateUrl: './alianzas.component.html',
 })
 export class AlianzasComponent {
-
+  banner = {
+    imageTitle: 'Banner Alianzas',
+    imageUrl: 'assets/banners/alianzas.jpg',
+    mainText: 'Alianzas',
+  }
+  alianzas = alianzas;
 }
